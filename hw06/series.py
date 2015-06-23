7fbd36126d0fb605bf4de4c9895d5866ab69bc5b
Sydney Decoto <sydney.decoto@gmail.com>
2015-06-23 01:02:07 -0700

    change code to allow for zero-based user input

--- hw06/series.py
+++ hw06/series.py
@@ -1,22 +1,20 @@
 
+
 # write Fibonacci series up to n
 def fibonacci(n):
     """The fibonacci function returns the nth values of the
     fibonacci series where n is a user-input number."""
-
     # create an array to populate
     fib = list(range(1, n + 2))
     # assign the first two values in the array
     fib[0] = 0
     fib[1] = 1
-
     # if the wants something beyond the first two values, calculate in loop
     if n > 2:
-        for i in range(2, n):
+        for i in range(2, n + 1):
             fib[i] = fib[i - 1] + fib[i - 2]
-
     # report the nth integer in the array.
-    print(fib[n - 1])
+    print(fib[n])
 
 
 def lucas(n):
@@ -27,14 +25,12 @@ def lucas(n):
     # assign the first two values in the array
     luc[0] = 2
     luc[1] = 1
-
     # if the wants something beyond the first two values, calculate in loop
     if n > 2:
-        for i in range(2, n):
+        for i in range(2, n + 1):
             luc[i] = luc[i - 1] + luc[i - 2]
-
     # report the nth integer in the array.
-    print(luc[n - 1])
+    print(luc[n])
 
 
 def sum_series(n, s1=0, s2=1):
@@ -48,32 +44,27 @@ def sum_series(n, s1=0, s2=1):
     # assign the first two values in the array
     ser[0] = s1
     ser[1] = s2
-
     # if the wants something beyond the first two values, calculate in loop
     if n > 2:
-        for i in range(2, n):
+        for i in range(2, n + 1):
             ser[i] = ser[i - 1] + ser[i - 2]
-
     # report the nth integer in the array.
-    print(ser[n - 1])
-
+    print(ser[n])
     if __name__ == "__main__":
         # Test fibonacci series
-        assert fibonacci(1) == 0
-        assert fibonacci(2) == 1
-        assert fibonacci(3) == 1
-        assert fibonacci(4) == 2
-
+        assert(fibonacci(0) == 0)
+        assert(fibonacci(1) == 1)
+        assert(fibonacci(2) == 1)
+        assert(fibonacci(3) == 2)
         # Test lucas series
-        assert lucas(1) == 2
-        assert lucas(2) == 1
-        assert lucas(3) == 3
-        assert lucas(4) == 4
-
+        assert lucas(0) == 2
+        assert lucas(1) == 1
+        assert lucas(2) == 3
+        assert lucas(3) == 4
         # Test sum_series
-        assert sum_series(1) == 0
+        assert sum_series(0) == 0
+        assert sum_series(1) == 1
         assert sum_series(2) == 1
-        assert sum_series(3) == 1
-        assert sum_series(1, 2, 1) == 2
-        assert sum_series(2, 2, 1) == 1
-        assert sum_series(3, 2, 1) == 3
+        assert sum_series(0, 2, 1) == 2
+        assert sum_series(1, 2, 1) == 1
+        assert sum_series(2, 2, 1) == 3
