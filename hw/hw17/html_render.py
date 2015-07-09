96bac6506a9dccebc624de0ad96992a25c8973e6
Sydney Decoto <sydney.decoto@gmail.com>
2015-07-08 18:58:05 -0700

    add element class

--- hw/hw17/html_render.py
+++ hw/hw17/html_render.py
@@ -7,5 +7,18 @@ Python class example.
 
 # The start of it all:
 # Fill it all in here.
+
+
 class Element(object):
-    pass
+
+    def __init__(self, name="", content=""):
+        self.name = name
+        self.content = content
+
+    def append(self, new_child):
+        self.content += new_child
+
+    def render(self, file_out, ind=""):
+        frontstr = "<" + self.name + ">\n"
+        endstr = "\n<" + self.name + "/>\n"
+        file_out.write(frontstr + self.content + endstr)
