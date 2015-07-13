0d7aa982db370b4a9696113f644baa53b71d43fc
Sydney Decoto <sydney.decoto@gmail.com>
2015-07-12 19:54:59 -0700

    add code from class on Wednesday

--- hw/hw17/html_render.py
+++ hw/hw17/html_render.py
@@ -13,12 +13,43 @@ class Element(object):
 
     def __init__(self, name="", content=""):
         self.name = name
-        self.content = content
+        # self.content = content
+        self.children = [content] if content else []
 
     def append(self, new_child):
-        self.content += new_child
+            self.children.append(new_child)
 
-    def render(self, file_out, ind=""):
-        frontstr = "<" + self.name + ">\n"
-        endstr = "\n<" + self.name + "/>\n"
-        file_out.write(frontstr + self.content + endstr)
+    def render(self, file_out, ind="    "):
+        self.indent = ind
+        IND_LEVEL = "    "
+        # append the html thingies, indent, and content and write file
+        file_out.write("%s<%s>" % (self.indent, self.name))
+        for child in self.children:
+            if (type(child) == str):
+                file_out.write("\n" + self.indent + IND_LEVEL + child)
+            else:
+                # Add new child node by recursively rendering
+                child.render(file_out, self.indent + IND_LEVEL)
+        file_out.write("\n%s</%s>" % (self.indent, self.name))
+
+
+class Html(Element):
+
+    def __init__(self, content=""):
+        Element.__init__(self, name="html", content="<!DOCTYPE html\n")
+
+    def render(self, file_out, indent="    "):
+        file_out.write("<!DOCTYPE html>\n")
+        Element.render(self, file_out, "")
+
+
+class Body(Element):
+
+    def __init__(self, content=""):
+        Element.__init__(self, name="body", content=content)
+
+
+class P(Element):
+
+    def __init__(self, content=""):
+        Element.__init__(self, name="p", content=content)
